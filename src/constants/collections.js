2db658a94f7a01987db31c40806a60326a02be13
davitads <51969228+daviopen@users.noreply.github.com>
2026-08-25 14:09:07 -0300

    refactor: centralize collection names

---
+++ src/constants/collections.js
@@ -0,0 +1,18 @@
+/** Nomes canônicos de collections. Evita strings espalhadas pelo código. */
+export const COLLECTIONS = Object.freeze({
+  USERS: 'users',
+  MINISTRY_FUNCTIONS: 'ministryFunctions',
+  USER_FUNCTIONS: 'userFunctions',
+  PERMISSIONS: 'permissions',
+  EVENTS: 'events',
+  UNAVAILABILITY: 'unavailability',
+  SCHEDULES: 'schedules',
+  SCHEDULE_MEMBERS: 'scheduleMembers',
+  SETLISTS: 'setlists',
+  SETLIST_SONGS: 'setlistSongs',
+  MUSICS: 'musicas',
+  SONGS: 'songs',
+  SONG_MINISTER_KEYS: 'songMinisterKeys',
+  AUDIT_LOGS: 'auditLogs',
+  LGPD_CONSENTS: 'lgpdConsents'
+});
