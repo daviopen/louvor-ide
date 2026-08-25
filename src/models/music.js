6b6f93cbaa3a7c1f8373d6864aae8646e938e5fe
davitads <51969228+daviopen@users.noreply.github.com>
2026-08-25 14:09:51 -0300

    refactor: add music model contract

---
+++ src/models/music.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} Music
+ * @property {string=} id
+ * @property {string} titulo
+ * @property {string|null=} artista
+ * @property {string|null=} tom
+ * @property {number|null=} bpm
+ * @property {string|null=} link
+ * @property {string} cifra
+ * @property {string|null=} letra
+ * @property {string[]=} ministros
+ * @property {Object<string,string>=} tomMinistro
+ * @property {Date|number=} criadoEm
+ * @property {Date|number=} updatedAt
+ */
+
+export const MUSIC_MODEL_VERSION = 1;
